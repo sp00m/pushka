12241a3997be252c5679ea6440a0232514fdf241
Christophe <christophe@qudini.com>
2017-12-17 18:36:10 +0000

    Static generation

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-require("./server/");
+const { promisify } = require("util");
+
+const fs = require("fs");
+const mkdirp = promisify(require("mkdirp"));
+const ncp = promisify(require("ncp"));
+const pug = require("pug");
+const rimraf = promisify(require("rimraf"));
+
+const readDir = promisify(fs.readdir);
+const writeFile = promisify(fs.writeFile);
+
+const compile = (inputFile) => {
+  const html = pug.renderFile(inputFile);
+  const outputFile = inputFile
+    .replace("./pug/", "./dist/")
+    .replace(/\.pug$/, ".html");
+  const outputDir = outputFile
+    .replace(/[^/]+$/, "");
+  return mkdirp(outputDir)
+    .then(() => writeFile(outputFile, html));
+};
+
+const walk = (dir) => readDir(dir)
+  .then((files) => Promise.all(files
+    .filter((file) => !file.startsWith("_"))
+    .filter((file) => (/^[^.]+$|\.pug$/).test(file))
+    .map((file) => `${dir}/${file}`)
+    .map((file) => file.endsWith(".pug") ? compile(file) : walk(file))
+  ));
+
+rimraf("./dist")
+  .then(() => mkdirp("./dist/static"))
+  .then(() => ncp("./static", "./dist/static"))
+  .then(() => walk("./pug"))
+  .then(() => console.log("Built!"))
+  .catch((error) => console.error(error));
